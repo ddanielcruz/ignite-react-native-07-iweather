0f75f2326af15c6c6f8a6ed7b760e1cfad8a2076
Daniel Cruz <danielcunha54@gmail.com>
2024-06-13 15:57:59 -0300

    resolve build issue

--- src/utils/getNextDays.spec.ts
+++ src/utils/getNextDays.spec.ts
@@ -1,7 +1,7 @@
 import { getNextDays } from './getNextDays'
 
 describe('getNextDays', () => {
-  it('should be return the next five days', () => {
+  it('should return the next five days', () => {
     const days = getNextDays()
     expect(days.length).toBe(5)
   })
